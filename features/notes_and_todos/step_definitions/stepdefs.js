9aea629d23816ae1486ba3055894a36a0b48aab5
Amulya Stha <sthaamulya@gmail.com>
2020-07-02 19:52:53 +0545

    added necesary structures to notes and todos directory

---
+++ features/notes_and_todos/step_definitions/stepdefs.js
@@ -0,0 +1,5 @@
+const assert = require("assert");
+const { Given, When, Then } = require("cucumber");
+const notesAndToDosModel = require("../rule_functions/rules");
+const notesAndToDos = new notesAndToDosModel();
+
