e8c033493abdc2be547d8a079d67167551ad0c96
Sama0307 <49469889+Sama0307@users.noreply.github.com>
2020-07-06 01:00:23 +0545

    delete notes

--- features/notes_and_todos/rule_functions/rules.js
+++ features/notes_and_todos/rule_functions/rules.js
@@ -1,4 +1,11 @@
 module.exports = class NotesAndToDos {
     addNote = (noteObject) => {
     }
+    deleteNote = (noteStatus) => {
+        const deleteNoteObject = {
+            currentStatus: 'deleted',
+            reponseMessage: 'Successfully note deleted'
+        }
+        return deleteNoteObject;
+    }
 }
--- features/notes_and_todos/step_definitions/stepdefs.js
+++ features/notes_and_todos/step_definitions/stepdefs.js
@@ -2,3 +2,26 @@ const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
 const notesAndToDosModel = require("../rule_functions/rules");
 const notesAndToDos = new notesAndToDosModel();
+/**
+ *  @story = Delete Note
+ *  @storyId = US39
+ */
+Given(
+    "The note on dashboard appears as {string}",
+    function (noteStatus) {
+        this.deleteNote = noteStatus;   
+    }
+);
+ 
+When("Clicks on delete button to {string}", function (noteStatus
+) {
+    const deleteNoteObject = notesAndToDos.deleteNote(noteStatus);
+    this.actualStatus = deleteNoteObject.currentStatus;
+    this.actualResponse = deleteNoteObject.reponseMessage;
+});
+ 
+Then("The note from the dashboard is {string} with message as {string}", function (expectedStatus,expectedMessage) {
+    assert.equal(this.actualResponse, expectedMessage);
+    assert.equal(this.actualStatus, expectedStatus);
+    console.log(expectedMessage);
+});
