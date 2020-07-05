f9779c4922d58d47a3a0474dcb69cbd3c81aa75f
shristi7690 <47600941+shristi7690@users.noreply.github.com>
2020-07-05 22:23:51 +0545

    Notes and To-dos feature Test

--- features/notes_and_todos/rule_functions/rules.js
+++ features/notes_and_todos/rule_functions/rules.js
@@ -1,4 +1,18 @@
 module.exports = class NotesAndToDos {
-    addNote = (noteObject) => {
+    updateNote = (updateNoteObject) => {
+        if (updateNoteObject.newNoteTitle === "")
+        {
+            return 'Title seems to be missing'
+        }
+
+        else if (updateNoteObject.newNoteContent === "")
+        {
+            return 'Content seems to be missing'
+
+        }
+
+        else {
+            return 'Successfully edited'
+        }
     }
 }
--- features/notes_and_todos/step_definitions/stepdefs.js
+++ features/notes_and_todos/step_definitions/stepdefs.js
@@ -3,3 +3,32 @@ const { Given, When, Then } = require("cucumber");
 const notesAndToDosModel = require("../rule_functions/rules");
 const notesAndToDos = new notesAndToDosModel();
 
+/**
+ *  @story = Update notes
+ *  @storyId = US22
+ */
+
+Given(
+    "The note appears on dashboard as {string}, {string}",
+    function (initialNoteTitle, initialNoteContent) {
+      this.initialNote = initialNoteTitle, initialNoteContent;
+    }
+  );
+
+  When("The initial notes is replaced with {string}, {string} and clicked on save button", function (
+    newNoteTitle, newNoteContent
+  ) {
+      const updateNoteObject = {
+        newNoteTitle: newNoteTitle,
+        newNoteContent: newNoteContent
+    };
+      this.actualOutcome = notesAndToDos.updateNote(updateNoteObject);
+  });
+
+  Then("The updted note is saved with {string}", function (expectedOutcome) {
+    assert.equal(this.actualOutcome, expectedOutcome);
+    if (this.actualOutcome === expectedOutcome) {
+      console.log(expectedOutcome);
+    }
+  });
+  
