9dad5e840e812c0bd704cddc104b9f679700a087
tmgdiskhya <57738790+tmgdiskhya@users.noreply.github.com>
2020-07-05 22:46:47 +0545

    modified features

--- features/notes_and_todos/rule_functions/rules.js
+++ features/notes_and_todos/rule_functions/rules.js
@@ -1,4 +1,14 @@
 module.exports = class NotesAndToDos {
+
     addNote = (noteObject) => {
+        if (noteObject.noteContent === "") {
+            return 'Content seems to be missing'
+        }
+        else if (noteObject.noteTitle === "") {
+            return 'Title seems to be missing'
+        }
+        else {
+            return 'Note has been successfully saved!'
+        }
     }
 }
--- features/notes_and_todos/step_definitions/stepdefs.js
+++ features/notes_and_todos/step_definitions/stepdefs.js
@@ -3,3 +3,30 @@ const { Given, When, Then } = require("cucumber");
 const notesAndToDosModel = require("../rule_functions/rules");
 const notesAndToDos = new notesAndToDosModel();
 
+/**
+ *  @story = Add new Note
+ *  @storyId = US21
+ */
+Given(
+    "The clean dashboard displays",
+    function () {
+        // Only shows desc.
+    }
+);
+
+When("The clean dashboard filled with {string}, {string} and clicked on save button", function (
+    noteTitle, noteContent
+) {
+    const noteObject = {
+        noteTitle: noteTitle,
+        noteContent: noteContent
+    };
+    this.actualOutcome = notesAndToDos.addNote(noteObject);
+});
+
+Then("The note is saved with message as {string}", function (expectedOutcome) {
+    assert.equal(this.actualOutcome, expectedOutcome);
+    if (this.actualOutcome === expectedOutcome) {
+        console.log(expectedOutcome);
+    }
+});
